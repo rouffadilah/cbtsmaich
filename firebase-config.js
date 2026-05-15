d58315c4da51fe4b084df4e85eb05d39dea6ea8e
rouffadilah <rouf.fadilah30@guru.sma.belajar.id>
2026-05-15 16:18:53 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,13 +4,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8R0VNO0noUlkcUcjBkpsGFrYPdtA7KxM",
-  authDomain: "cbt-sekolah-7fed0.firebaseapp.com",
-  projectId: "cbt-sekolah-7fed0",
-  storageBucket: "cbt-sekolah-7fed0.firebasestorage.app",
-  messagingSenderId: "289218396137",
-  appId: "1:289218396137:web:366383efd1348edad3d578",
-  measurementId: "G-GF6PJWK2S5"
+    apiKey: "AIzaSyB8R0VNO0noUlkcUcjBkpsGFrYPdtA7KxM",
+    authDomain: "cbt-sekolah-7fed0.firebaseapp.com",
+    projectId: "cbt-sekolah-7fed0",
+    storageBucket: "cbt-sekolah-7fed0.firebasestorage.app",
+    messagingSenderId: "289218396137",
+    appId: "1:289218396137:web:366383efd1348edad3d578",
+    measurementId: "G-GF6PJWK2S5"
 };
 
 const app = initializeApp(firebaseConfig);
